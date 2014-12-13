bac8d1db0690748fceed6a489fa99f5f906e8fde
JackieGitHub78 <jakelin11@hotmail.com>
2014-12-13 11:18:27 -0600

    ultima actualizacion clase

--- js/actions.js
+++ js/actions.js
@@ -1,46 +1,35 @@
 var fn = {
-ready: function()
-    {
-        document.addEventListener("deviceready",fn.init,false);
+    ready: function(){
+        document.addEventListener('deviceready',fn.init,false);
     },
-    
-init: function()
-    {
-       $('#regSend').click(fn.enviarRegistro);
+    init: function(){
+        $('#regSend').click(fn.enviarRegistro);
         $('#regFoto').click(capture.imgCapture);
         var x = false;
         if(!x)
-        {
-            window.location.href ='#reg';
-        }
+            window.location.href = '#reg';
     },
-    
-    enviarRegistro: function()
-    {
+    enviarRegistro: function(){
         var nom = $('#regNom').val();
         var tel = $('#regTel').val();
         var mail = $('#regMail').val();
         var foto = $('#regFoto').attr('rel');
         
-        if(nom != '' && tel != '' && mail != '' && foto != '' && foto !=undefined)
-            {
-            $.ajax(
-                {
-                  type: "POST",
-                  url: "http://carlos.igitsoft.com/apps/test.php",
-                  data: { nom: nom, mail: mail, tel:tel }
+        if(nom != '' && tel != '' && mail != '' && foto != '' && foto != undefined){
+            $.ajax({
+                type: "POST",
+                url: "http://carlos.igitsoft.com/apps/test.php",
+                data: { nom: nom, mail: mail, tel: tel }
+            })
+            .done(function( msg ) {
+                if(msg == 1){
+                    fileTransfer.subirFoto();
                 }
-            )
-                  .done(function( msg ) {
-                    alert( "Data Saved: " + msg );
-                  });
-            }
-                else
-                    {
-                        navigator.notification.alert('todos los campos son requeridos', null,'Lo sentimos', 'aceptar');
-                    }
+            });
+        }else{
+            navigator.notification.alert('Todos los campos son requeridos',null,'Lo sentimos','Aceptar');
+        }
     }
-    
-}; 
+};
 
 $(fn.ready);
